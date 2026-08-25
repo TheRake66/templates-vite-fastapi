832d82bdeb315ed015c1a3397f23b0d508b84341
TheRake66 <none>
2026-08-25 04:03:22 +0200

    add useffect

--- template_page/src/pages/{{namespace_ossep_lower}}.tsx
+++ template_page/src/pages/{{namespace_ossep_lower}}.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate, useParams } from "react-router-dom";
 import styles from './{{lower_name}}.module.scss';
